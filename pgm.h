9477e7c9d3a1f998981597e6e68ac93acf06925b
DottorBooom <94912692+DottorBooom@users.noreply.github.com>
2023-12-13 19:06:39 +0100

    File and library for .PGM image

---
+++ pgm.h
@@ -0,0 +1,26 @@
+#ifndef _PGM_H
+#define _PGM_H
+
+#include <stdio.h>
+
+struct _pgm_image {
+  int width;
+  int height;
+  int offset;
+  int size;
+  FILE * fd;
+  char * data;
+};
+
+typedef struct _pgm_image pgm;
+typedef struct _pgm_image * pgm_ptr;
+
+int open_image(char * path, pgm_ptr img);
+
+int empty_image(char * path, pgm_ptr img, int width, int height);
+
+char * pixel_at(pgm_ptr img, int x, int y);
+
+int close_image(pgm_ptr img);
+
+#endif
