05211fcc434c5710b05e91d94cc0cfbaf96ff4cc
DottorBooom <94912692+DottorBooom@users.noreply.github.com>
2023-12-13 19:05:17 +0100

    V1.0.0: First version of the working program
    First stable version of the project. Each function performs its task correctly, and all errors have been handled.

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 #include "util.h"
 #include "mandelbrot.h"
+#include "pgm.h"
 
 int main(int argc, char * argv[]){
 
@@ -10,10 +14,10 @@ int main(int argc, char * argv[]){
     printf("Program started. Computing input arguments...");
 
     int * components = (int *)malloc(3 * sizeof(int));
-    int err = argv_check(argc,argv,components);
+    int err = argv_check(argc,argv,components); //divido e variabili singole?
 
     if(err != 0){
-        printf("...program end with error %d\n",err);
+        printf("...program end with error %d.\n",err);
         return 0;
     }else{
         printf("...values initialized correctly:\n");
@@ -21,21 +25,52 @@ int main(int argc, char * argv[]){
     }
     //--------------------------------------------------------------//
     printf("Creating an equalspaced array for real part and immaginary part...");
-    float * r_rows = linspace(-2,1,components[1]);
-    float * i_cols = linspace(1,-1,components[2]);
-    if(r_rows == 0 || i_cols == 0){
-        printf("...program end with error -5\n");
+    float * imm = linspace(-1,1,components[1]);
+    float * real = linspace(-2,1,components[2]);
+    if(real == 0 || imm == 0){
+        printf("...program end with error -5.\n");
         return 0;
     }
     printf("...arrays line spaced correctly.\n");
     //--------------------------------------------------------------//
-    printf("Start computing the mandelbrot set...");
-    float r = 2;
-    float * min_n = mandelbrot_set(components,r_rows,i_cols,r);
-
+    printf("Start computing the mandelbrot set..."); //perché non lo esegue? O3?
+    float * min_n = mandelbrot_set(components,real,imm);
+    free(real); //Meglio alla fine?
+    free(imm);
     printf("...values computed correctly.\n");
-
     //--------------------------------------------------------------//
+    printf("Inizialazing image space...");
+    pgm image;
+    err = empty_image(strcat(argv[1],".pgm"), &image, components[2], components[1]);
+    if (err != 0) {
+        printf("...unable to open image. Program end with error %d.\n", err);
+        free(components);
+        free(min_n);
+        return 0;
+    }else{
+        printf("...mapped completed. Rendering pixel...");
+    }
+    for (int y = 0; y < components[1]; y++) { //Dovrei spostarlo così gestisco l'errore?
+        for (int x = 0; x < components[2]; x++) {
+                char * c = pixel_at(&image, x, y);
+                if (c == NULL) {
+                    printf("...error at x = %d y = %d.\n", x, y);
+                }else{
+                    *c = (int)(255*log(min_n[components[2]*y+x])/(float)log(components[0]));
+                }
+            }
+    }
+    printf("...rendering completed. Image saved correctly.\n");
+    err = close_image(&image);
+    if(err != 0){
+        printf("...unable to close image correctly. Program end with error %d.\n", err);
+        free(components);
+        free(min_n);
+        return 0;
+    }
+    //--------------------------------------------------------------//
+    free(components);
+    free(min_n);
     printf("End of the program.\n");
     return 0;
 }
