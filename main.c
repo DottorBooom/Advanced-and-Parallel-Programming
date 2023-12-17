7a02a5b41b99fb28d2dceb66943272273af7316a
DottorBooom <94912692+DottorBooom@users.noreply.github.com>
2023-12-18 00:54:31 +0100

    v2.0.0: Speeded up version
    Several methods have been added to make the code faster. The computation of values and image rendering have been parallelized using OpenMP APIs, and only half of the values are calculated by exploiting the symmetry at y=0 of the fractal, effectively halving the computation time. This comes at the expense of the image rendering complexity, which starts from both the first and last rows simultaneously until reaching the center; if the number of rows is odd, the central row needs to be calculated separately, otherwise, it behaves normally. Other minor optimizations have been made, such as reducing the bit size for certain matrix types, significantly reducing the space occupied and allowing more values to be stored in the cache.

--- main.c
+++ main.c
@@ -4,78 +4,89 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <time.h>
+#include <omp.h>
+#include <stdint.h>
 
 #include "util.h"
 #include "mandelbrot.h"
 #include "pgm.h"
 
+int time_stamp(int * components, int repetitions, char * name){
+    
+    double t = 0;
+    
+    for (int i = 0; i < repetitions; i++) {
+        
+        double start = omp_get_wtime();
+
+        float * imm = linspace(-1,1,components[1]);
+        float * real = linspace(-2,1,components[2]);
+        
+        if(real == 0 || imm == 0){
+            printf("Program end with error -5.\n");
+            free(real);
+            free(imm);
+            free(components);
+            return -5;
+        }
+
+        //--------------------------------------------------------------//
+        
+        uint16_t * min_n = mandelbrot_set(components,real,imm);
+        free(real);
+        free(imm);
+        
+        //--------------------------------------------------------------//
+
+        pgm image;
+        int8_t err = empty_image(strcat(name,".pgm"), &image, components[2], components[1]);
+        if (err != 0) {
+            printf("Unable to open image. Program end with error %d.\n", err);
+            free(components);
+            free(min_n);
+            return err;
+        }
+
+        print_image(components, &image, min_n);
+
+        err = close_image(&image);
+        if(err != 0){
+            printf("Unable to close image correctly. Program end with error %d.\n", err);
+            free(components);
+            free(min_n);
+            return err;
+        }
+
+        double end = omp_get_wtime();
+        t += (end - start);
+        printf("Finito test %d\n",i);
+    }
+
+    printf("Tempo per esecuzione standard: %f ms\n", 1000 * t / repetitions);
+    return 0;
+}
+
 int main(int argc, char * argv[]){
 
     //--------------------------------------------------------------//
-    printf("Program started. Computing input arguments...");
-
-    int * components = (int *)malloc(3 * sizeof(int));
-    int err = argv_check(argc,argv,components); //divido e variabili singole?
+    int * components = (int *)malloc(4 * sizeof(int));
+    int8_t err = argv_check(argc,argv,components);
 
     if(err != 0){
-        printf("...program end with error %d.\n",err);
-        free(components);
-        return 0;
-    }else{
-        printf("...values initialized correctly:\n");
-        printf("+ M = %d\n+ Number of rows = %d\n+ Number of columns = %d\n",components[0],components[1],components[2]);
-    }
-    //--------------------------------------------------------------//
-    printf("Creating an equalspaced array for real part and immaginary part...");
-    float * imm = linspace(-1,1,components[1]);
-    float * real = linspace(-2,1,components[2]);
-    if(real == 0 || imm == 0){
-        printf("...program end with error -5.\n");
-        free(real); //Meglio alla fine?
-        free(imm);
+        printf("Program end with error %d.\n",err);
         free(components);
         return 0;
     }
-    printf("...arrays line spaced correctly.\n");
     //--------------------------------------------------------------//
-    printf("Start computing the mandelbrot set..."); //perché non lo esegue? O3?
-    float * min_n = mandelbrot_set(components,real,imm);
-    free(real); //Meglio alla fine?
-    free(imm);
-    printf("...values computed correctly.\n");
-    //--------------------------------------------------------------//
-    printf("Inizialazing image space...");
-    pgm image;
-    err = empty_image(strcat(argv[1],".pgm"), &image, components[2], components[1]);
-    if (err != 0) {
-        printf("...unable to open image. Program end with error %d.\n", err);
-        free(components);
-        free(min_n);
-        return 0;
-    }else{
-        printf("...mapped completed. Rendering pixel...");
-    }
-    for (int y = 0; y < components[1]; y++) { //Dovrei spostarlo così gestisco l'errore?
-        for (int x = 0; x < components[2]; x++) {
-                char * c = pixel_at(&image, x, y);
-                if (c == NULL) {
-                    printf("...error at x = %d y = %d.\n", x, y);
-                }else{
-                    *c = (int)(255*log(min_n[components[2]*y+x])/(float)log(components[0]));
-                }
-            }
-    }
-    printf("...rendering completed. Image saved correctly.\n");
-    err = close_image(&image);
+    int repetitions = 5;
+    err = time_stamp(components, repetitions, argv[1]);
     if(err != 0){
-        printf("...unable to close image correctly. Program end with error %d.\n", err);
         free(components);
-        free(min_n);
         return 0;
     }
     //--------------------------------------------------------------//
     free(components);
-    free(min_n);
     printf("End of the program.\n");
     return 0;
 }
